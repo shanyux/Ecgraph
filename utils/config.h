9c4aab3e05e8d9ef85a2a161c4a9b4e824efd5d8
shanyux <syx1558@163.com>
2017-06-21 09:51:04 +0800

    Third

--- utils/config.h
+++ utils/config.h
@@ -1,25 +1,3 @@
-/*
- *
- * author:      allen lee(Junhao Li) allen_lee922@foxmail.com
- * address:     WNLO, Huazhong University of Science & Technology
- * Time:        2016.11
- *
- * dist-xs
- *
- * Copyright 2015 Key Laboratory of Data Storage System, Ministry of Education WNLO HUST
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 #ifndef _CONFIG_
 #define _CONFIG_
 #include <cassert>
--- utils/graph_data.h
+++ utils/graph_data.h
@@ -1,30 +1,4 @@
-﻿/*
- * author:      allen lee(Junhao Li) allen_lee922@foxmail.com
- * address:     WNLO, Huazhong University of Science & Technology
- * Time:        2016.11
- *
- * Copyright 2015 Key Laboratory of Data Storage System, Ministry of Education WNLO HUST
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- *  This namespace dx_lib includes somes utils for our project.
- *  Class graph_data is a graph data abstract class
- *
- *
- */
-#ifndef _GRAPH_DATA_H_
+﻿#ifndef _GRAPH_DATA_H_
 #define _GRAPH_DATA_H_
 
 #include <sstream>
